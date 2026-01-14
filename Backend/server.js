d30054ef898263e1276550952ce447a0495db83c
Avilah Orpha <sdafchennai@gmail.com>
2026-01-14 17:45:53 +0530

    Initial backend setup for HiRize MVP

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", require("./routes/authroutes"));
+
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("HiRize API running");
+});
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.error(err));
